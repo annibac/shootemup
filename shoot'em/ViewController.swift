3d803ef43da321241358ca63b719db06423a9de0
theo <bequet_t@etna-alternance.net>
2017-07-24 15:37:55 +0200

    Mur qui defile, petit bug a régler dessus (bande blanche)

--- shoot'em/ViewController.swift
+++ shoot'em/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var spiderman: UIImageView!
+    @IBOutlet weak var spriteChar: UIImageView!
+    @IBOutlet weak var wall1: UIImageView!
+    @IBOutlet weak var wall2: UIImageView!
+    @IBOutlet weak var spiders: UIImageView!
     
     var timer: Timer!
     var timerSprite: Timer!
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         timerSprite = Timer.scheduledTimer(timeInterval: 0.13, target: self, selector: #selector(changeImg), userInfo: nil, repeats: true)
         throwSpiders(img: spiders)
+        
+        moveWalls(wall2)
+        moveWalls(wall1)
     }
 
 
@@ -30,10 +37,14 @@ class ViewController: UIViewController {
     @IBAction func moveButtonTD(_ sender: UIButton) {
         let screenSize = UIScreen.main.bounds
         timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (t:Timer) in
-        if (sender.tag) == 0 {
-            self.spiderman.center.x -= 10
-        } else {
-            self.spiderman.center.x += 10
+            if (sender.tag == 0) {
+                if (self.spriteChar.center.x - 10 > 0) {
+                        self.spriteChar.center.x -= 10
+                }
+            } else {
+                if (self.spriteChar.center.x + 10 < screenSize.width) {
+                        self.spriteChar.center.x += 10
+                }
             }
         })
     }
@@ -41,14 +52,39 @@ class ViewController: UIViewController {
     func changeImg()
     {
         spriteChar.image = UIImage(named: "\(imagePos).png")
-        if(imagePos >= 0 && imagePos != 5){
+        if(imagePos >= 0 && imagePos != 5) {
             imagePos += 1;
-        }
-        else
-        {
+        } else {
             imagePos = 0;
         }
     }
-     
+    
+    private func throwSpiders(img: UIImageView)
+    {
+        UIView.animate(withDuration: 0.5, animations: {
+            img.center.y = self.view.frame.minY
+        }, completion: { (true) in
+                img.center.y = self.spriteChar.center.y
+                img.center.x = self.spriteChar.center.x
+                self.throwSpiders(img: img)
+        })
+    }
+    
+    private func moveWalls(_ img: UIImageView) {
+        UIView.animate(withDuration: 5, delay: 0, options: .curveLinear, animations: {
+            if (img.tag == 0) {
+                img.center.y = (self.view.frame.size.height / 2)
+            } else if (img.tag == 1) {
+                img.center.y = self.view.frame.size.height + self.view.frame.size.height / 2
+            }
+        }, completion: { (true) in
+            if (img.tag == 0) {
+                img.center.y = -(self.view.frame.size.height / 2)
+            } else if (img.tag == 1) {
+                img.center.y = self.view.frame.size.height / 2
+            }
+            self.moveWalls(img)
+        })
+    }
 }
 
